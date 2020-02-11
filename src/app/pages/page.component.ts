31bab12a46f0b995f2721aa9bb9a67e945b7b916
TJ <talhajamil114@gmail.com>
2020-02-12 01:45:27 +0500

    TJ - Front end work

--- src/app/pages/page.component.ts
+++ src/app/pages/page.component.ts
@@ -1,15 +1,13 @@
-import {Component} from '@angular/core';
-import {AuthService} from '@services/common/auth.service';
+import { Component } from '@angular/core';
+import { AuthService } from '@services/common/auth.service';
 
 @Component({
-    selector: 'app-page',
-    templateUrl: './page.component.html',
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.css'],
 })
 export class PagesComponent {
+  isNavBarCollapsed = true;
 
-    constructor(public auth: AuthService) {
-
-    }
-
-
+  constructor(public auth: AuthService) {}
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,44 +1,37 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {PagesComponent} from './page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './page.component';
+import { NgbCollapseModule, NgbDropdownModule, NgbNav, NgbNavModule } from '@nodeMod/@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            {
-                path: '',
-                loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule)
-            },
-            {
-                path: 'booking',
-                loadChildren: () => import('./map/map.module').then(m => m.MapModule)
-            },
-            {
-                path: 'feedback',
-                loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule)
-            },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'main' },
+      {
+        path: 'main',
+        loadChildren: () => import('./booking/booking.module').then((m) => m.BookingModule),
+      },
+      {
+        path: 'booking',
+        loadChildren: () => import('./map/map.module').then((m) => m.MapModule),
+      },
+      {
+        path: 'feedback',
+        loadChildren: () => import('./feedback/feedback.module').then((m) => m.FeedbackModule),
+      },
 
-            {
-                path: 'dashboard',
-                loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then(m => m.AdminDashboardModule)
-            },
-        ]
-    }
-
-
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then((m) => m.AdminDashboardModule),
+      },
+    ],
+  },
 ];
 
-
 @NgModule({
-    declarations: [PagesComponent],
-    imports: [
-        RouterModule.forChild(routes),
-    ]
+  declarations: [PagesComponent],
+  imports: [RouterModule.forChild(routes), NgbCollapseModule, NgbDropdownModule],
 })
-export class PagesModule {
-}
-
-
-
+export class PagesModule {}
